806951e65a00f7ab7e027342dbecc26dec242dee
neslihanaydin <neslihanaydin43@gmail.com>
2019-04-27 17:29:48 +0300

    getDizinYolu() methodu eklendi

--- sha1ozet.py
+++ sha1ozet.py
@@ -2,7 +2,26 @@ from hashlib import sha1
 import sys
 import os
 import codecs
+import subprocess
 
+def getDizinYolu(baslangicDizinYolu):
+	subDir=[]
+	yol = subprocess.check_output(["ls",baslangicDizinYolu])
+	yol = str(yol)
+	tempYol = ""
+	for i in range(len(yol)):
+		if (i>1 and i< (len(yol)-3)):
+			tempYol+=yol[i]
+	print("---------------------")
+	baslangicDizinYolu+=tempYol
+	#print("baslangicDizinYolu" + baslangicDizinYolu)
+	for subdir in os.listdir(baslangicDizinYolu):
+		subDir.append(subdir)
+	dondur = (baslangicDizinYolu+"/"+subDir[1])
+	return dondur
+	
+
+	
 
 def getSha1(filePath):
     m = sha1()
@@ -15,9 +34,18 @@ def getSha1(filePath):
         print ("File Not Found")
         
 ozetYolu = r"/home/neslihan/Masaüstü/ozetdosyasi"
-dizinYolu2= r"/home/neslihan/Masaüstü/ozet1"
-dizinYolu1 = r"/run/user/1000/gvfs/mtp:host=%5Busb%3A003%2C010%5D/Dahili olarak paylaşılan depolama alanı/system"
-dizinYolu = r"/run/user/1000/gvfs/mtp:host=%5Busb%3A003%2C004%5D/Dahili olarak paylaşılan depolama alanı/Android/data/com.instagram.android"
+#dizinYolu2= r"/home/neslihan/Masaüstü/ozet1"
+#dizinYolu1 = r"/run/user/1000/gvfs/mtp:host=%5Busb%3A003%2C010%5D/Dahili olarak paylaşılan depolama alanı/system"
+#dizinYolu = r"/run/user/1000/gvfs/mtp:host=%5Busb%3A003%2C004%5D/Dahili olarak paylaşılan depolama alanı/Android/data/com.instagram.android"
+
+baslangicDizinYolu = r"/run/user/1000/gvfs/"
+try:
+	subDirSha1 = str(getDizinYolu(baslangicDizinYolu))
+	dizinYolu = subDirSha1+"/Android/data"
+	#print("dizinYolu "+dizinYolu)
+except Exception as e:
+	print("USB baglantisini kontrol edin !")
+	dizinYolu = ""
 
 if (os.path.isfile(ozetYolu))==False:
     ozetDosyasi = open(ozetYolu,"a")
@@ -30,8 +58,6 @@ try:
 		i=i+1
 		yazilacak=""
 		for name in files:
-			print(type(files))
-			break
 			yol = os.path.join(root, name)
 			try:
 				ozet = getSha1(yol)
